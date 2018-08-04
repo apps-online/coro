e652ced3b80b7f3fe7f0fb25018be6369d709696
AppsOnline <42091234+apps-online@users.noreply.github.com>
2018-08-04 14:24:30 +0200

    add ExceptionApp

---
+++ src/Exception/AppException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @name AppException
+ * @version 1.0.0
+ * @author Miroslaw Kukuryka
+ * @copyright (c) 2018 (https://www.appsonline.eu)
+ * @link https://www.appsonline.eu
+ */
+namespace Coro\Exception;
+
+class AppException implements ExceptionInterface
+{
+}
